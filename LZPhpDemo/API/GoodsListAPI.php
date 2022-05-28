ce63759e22e3a9291c1bf7d3890d1e8e1b36a44a
zhanglizhi <zhang_zhi_li@163.com>
2022-05-28 17:12:09 +0800

    --

--- LZPhpDemo/API/GoodsListAPI.php
+++ LZPhpDemo/API/GoodsListAPI.php
@@ -1,15 +1,23 @@
 <?php
 include '../Server/db.php';
 include '../Server/ResponseJson.php';
+header('Access-Control-Allow-Origin:http://localhost:3000'); // *代表允许任何网址请求
 
 //echo  $_GET;
 //var_dump($_GET);
+$goodsid = 0;
+if (!empty($_GET["id"]) ){
+    $goodsid = $_GET["id"];
+}
 
 //$dsn = "mysql:dbname=LZPhpDemo;host=localhost";
 //
 //$pdo = new PDO($dsn,'root','');
 //$sql = "INSERT INTO `goods` (`id`, `title`) VALUES (NULL, '56');";
 $sql = "SELECT * FROM `goods`;";
+if ($goodsid != 0){
+    $sql = "SELECT * FROM `goods` WHERE id = $goodsid;";
+}
 $sth = $pdo->prepare($sql);
 $isSuccess =  $sth->execute();
 //var_dump($sth);
@@ -23,3 +31,5 @@ if ($isSuccess){
 }
 
 echo  $apiResult;
+
+
